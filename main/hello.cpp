df3a6eaf857e5b8ab5d35e6b05c89632a5c6e10c
nishadev9 <76961173+nishadev9@users.noreply.github.com>
2023-02-17 19:23:29 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "Made changes \n";
+  cout << "Made changes 506 \n";
   return 0;
 }
